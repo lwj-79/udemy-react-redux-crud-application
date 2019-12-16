6de660faf6d78ee774e5ebd618cfd524c3389826
lwj <lwj@lwjMBC.local>
2019-12-16 16:04:06 +0900

    update bug

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import * as serviceWorker from './serviceWorker';
 const store = createStore(reducer);
 
 ReactDOM.render(
-    <Provider store={sotre}><App /></Provider>, 
+    <Provider store={store}><App /></Provider>, 
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
 import { combineReducers } from 'redux';
 import count from './count';
 
-export default combineReducers({ counter })
+export default combineReducers({ count })
