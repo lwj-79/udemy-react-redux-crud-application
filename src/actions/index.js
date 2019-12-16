e8c812e1eaebe7ba73563e4deb9b57076c17b1fc
lwj <lwj@lwjMBC.local>
2019-12-16 15:16:02 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+const INCREMENT = 'INCREMENT';
+const DECTEMENT = 'DECREMENT';
+
+export const increment = () => ({
+    type: INCREMENT
+})
+
+
+export const decrement = () =>({
+    type: DECREMENT
+})
