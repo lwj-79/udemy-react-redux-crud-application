762998e3827d1674fb0ef1e693585a041df80f9c
lwj <lwj@lwjMBC.local>
2019-12-19 16:39:36 +0900

    implement event update

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,13 +2,20 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
-//import { postEvent } from '../actions'
+import { deleteEvent, getEvent, putEvent } from '../actions'
 
 class EventsShow extends Component{
   constructor(props) {
     super(props);
     this.onSubmit = this.onSubmit.bind(this);
+    this.onDeleteClick = this.onDeleteClick.bind(this);
   }
+
+  componentDidMount(){
+    const { id } = this.props.match.params;
+    if (id) this.props.getEvent(id);
+  }
+
   renderField(field){
     const { input, label, type, meta:{ touched, error } } = field;
     
@@ -19,19 +26,27 @@ class EventsShow extends Component{
       </div>
     );
   }
+
+  async onDeleteClick(){
+    const { id } = this.props.match.params
+    await this.props.deleteEvent(id)
+    this.props.history.push('/');
+  }
+
   async onSubmit(values) {
-    //await this.props.postEvent(values);
+    await this.props.putEvent(values);
     this.props.history.push('/');
   }
   render(){
-    const { handleSubmit, pristine, submitting } = this.props
+    const { handleSubmit, pristine, submitting, invalid } = this.props
     return(
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label='Title' name='title' type='text' component={this.renderField}/></div>
         <div><Field label='Body' name='body' type='text' component={this.renderField} /></div>
         <div>
-          <input type='submit' value='submit' disabled={pristine || submitting} />
-          <Link to='/'>Cancel</Link>
+          <input type='submit' value='submit' disabled={pristine || submitting || invalid } />
+          <Link to='/'>Cancel</Link>&nbsp;
+          <Link to='/' onClick={this.onDeleteClick}>Delete</Link>
         </div>
       </form>
     );
@@ -41,11 +56,14 @@ class EventsShow extends Component{
 const validate = values => {
   const errors = {}
   if(!values.title) errors.title = 'Enter a title, Please';
-  if(!values.title) errors.body = 'Enter a body, Please';
+  if(!values.body) errors.body = 'Enter a body, Please';
   return errors;
 }
-
-//const mapDispatchToProps = ({postEvent});
-export default connect(null, null)(
+const mapStateToProps = (state, ownProps) => {
+  const event = state.events[ownProps.match.params.id]
+  return { initialValues: event, state }
+};
+const mapDispatchToProps = ({deleteEvent, putEvent, getEvent});
+export default connect(mapStateToProps, mapDispatchToProps)(
   reduxForm({ validate, form:'eventShowForm'})(EventsShow)
 )
